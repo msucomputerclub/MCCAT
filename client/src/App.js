2e03b6cdeaf851815815a0a90b06ea017f618eb6
Cristian Novac <crysist14@gmail.com>
2019-04-12 15:55:03 -0400

    basic react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    meetingDate: ""
+  };
+  componentDidMount() {
+    fetch(
+      `http://localhost:5000/api/meeting/${new Date().toLocaleDateString()}`
+    )
+      .then(response => response.json())
+      .then(result => {});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
