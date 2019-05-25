a585d0d562a685f60ef29453ac1db283a0cd05fc
Cristian Novac <novacc1@montclair.edu>
2019-05-25 18:23:29 -0400

    fixed no match crashing

--- validation/clean-cwid.js
+++ validation/clean-cwid.js
@@ -1,6 +1,10 @@
 function cleanCWID(cwid) {
   //clean user input in case the cwid was entered through id swipe or manual
-  var match = cwid.match(/(^;\d{9}(?:=))|(^M\d+)|(^\d{8,9})/g); //match student id card or manual
+  try{
+    var match = cwid.match(/(^;\d{9}(?:=))|(^M\d+)|(^\d{8,9})/g); //match student id card or manual
+  }catch(e){
+    var match="";
+  }
   if (match) {
     return match[0]
       .replace(/\D/g, "") //remove non digit characters
